98550b3d28d8accd5d3e1da242692f9b3d0721fa
risano <muhammadrisano@gmail.com>
2020-12-10 17:00:44 +0700

    oke

--- src/main.js
+++ src/main.js
@@ -6,6 +6,32 @@ import store from './store'
 
 Vue.config.productionTip = false
 
+// Vue.directive('setfont', {
+//   bind: function (el, binding) {
+//     console.log(binding.modifiers.style)
+//     // if (binding.value === 'blue') {
+//     //   el.style.color = 'blue'
+//     // } else if (binding.value === 'red') {
+//     //   el.style.color = 'red'
+//     // } else {
+//     //   el.style.color = 'salmon'
+//     // }
+//     el.style.color = binding.value ? binding.value : 'black'
+//     // kondisi ke 2 => arg
+//     if (binding.arg === 'big') {
+//       el.style.fontSize = '50px'
+//     } else if (binding.arg === 'small') {
+//       el.style.fontSize = '8px'
+//     } else {
+//       el.style.fontSize = '13px'
+//     }
+
+//     // kondisi ke 3 => modifiers
+//     if (binding.modifiers.style) {
+//       el.style.fontStyle = 'italic'
+//     }
+//   }
+// })
 new Vue({
   router,
   store,
--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,5 +1,6 @@
 <template>
 <div class="container">
+  <h2 v-setfont:big.style="'yellow'">halaman home</h2>
   <div class="card" v-for="product in getProduct" :key="product.id">
   <img src="#" class="card-img-top" alt="...">
   <div class="card-body">
@@ -27,6 +28,35 @@ export default {
   },
   mounted () {
     this.getAllProduct()
+  },
+  directives: {
+    setfont: {
+      bind: function (el, binding) {
+        console.log(binding.modifiers.style)
+        // if (binding.value === 'blue') {
+        //   el.style.color = 'blue'
+        // } else if (binding.value === 'red') {
+        //   el.style.color = 'red'
+        // } else {
+        //   el.style.color = 'salmon'
+        // }
+        // el.style.color = binding.value ? binding.value : 'black'
+        el.innerHTML = binding.value
+        // kondisi ke 2 => arg
+        if (binding.arg === 'big') {
+          el.style.fontSize = '50px'
+        } else if (binding.arg === 'small') {
+          el.style.fontSize = '8px'
+        } else {
+          el.style.fontSize = '13px'
+        }
+
+        // kondisi ke 3 => modifiers
+        if (binding.modifiers.style) {
+          el.style.fontStyle = 'italic'
+        }
+      }
+    }
   }
 }
 </script>
